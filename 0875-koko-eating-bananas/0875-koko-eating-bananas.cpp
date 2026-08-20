6a37459f141eab698724ad9c359f56b0c8acb871
Neeraj yadav <neerajyadav291701@gmail.com>
2026-08-20 20:16:12 +0530

    Time: 0 ms (100%), Space: 23 MB (56.62%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    long long calculateTotalHour(vector<int>& piles,int speed){
+   long long calculateTotalHour(vector<int>& piles,int speed){
         long long  totalH = 0;
         for(int bananas : piles){
-            totalH +=((long long)bananas + speed -1) / speed;
+           totalH += ((long long)bananas + speed - 1) / speed;
         }
         return totalH;
     }
@@ -14,7 +14,7 @@ public:
           int ans = maxElement;
         while(low<= high){
             int mid = low + (high - low)/2;
-          long long   total_hr = calculateTotalHour(piles,mid);
+          long long total_hr = calculateTotalHour(piles,mid);
             if(total_hr <= h){
                 ans = mid;
                 high = mid -1;
