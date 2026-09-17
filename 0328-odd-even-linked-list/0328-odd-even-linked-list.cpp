35df0b457b5f3ab047d1f94284e67b73d5e7646c
Neeraj yadav <neerajyadav291701@gmail.com>
2026-09-17 21:19:36 +0530

    Time: 0 ms (100%), Space: 15.5 MB (63%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -5,21 +5,21 @@ public:
         if (head == NULL || head->next == NULL) {
             return head;
         }
+        ListNode*odd = head;
+         ListNode*even = head->next;
+         ListNode*evenHead = even;
 
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
-
-        while (even != NULL && even->next != NULL) {
+         while(even != NULL && even->next != NULL){
             odd->next = even->next;
             odd = odd->next;
 
             even->next = odd->next;
             even = even->next;
-        }
+         } 
+         odd->next = evenHead;
+         return head;
 
-        odd->next = evenHead;
 
-        return head;
+
     }
 };
